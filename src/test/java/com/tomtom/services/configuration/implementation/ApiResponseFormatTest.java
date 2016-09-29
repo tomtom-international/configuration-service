bd3a7fe3c26cab6d1e9eb5649b2c93786f089114
Rijn Buve <rijn@buve.nl>
2016-09-29 13:46:23 +0200

    Fixed test

--- src/test/java/com/tomtom/services/configuration/implementation/ApiResponseFormatTest.java
+++ src/test/java/com/tomtom/services/configuration/implementation/ApiResponseFormatTest.java
@@ -5,46 +5,59 @@
 package com.tomtom.services.configuration.implementation;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.junit.Assert;
+import org.junit.After;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.core.MediaType;
+import javax.annotation.Nonnull;
 import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML_TYPE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @SuppressWarnings({"JUnitTestMethodWithNoAssertions", "OverlyBroadThrowsClause", "ProhibitedExceptionDeclared"})
 public class ApiResponseFormatTest {
     private static final Logger LOG = LoggerFactory.getLogger(ApiResponseFormatTest.class);
 
+    private LocalTestServer server = null;
+
+    public void startServer(@Nonnull final String config) throws IncorrectConfigurationException {
+        server = new LocalTestServer("classpath:" + config);
+        server.startServer();
+    }
+
+    @After
+    public void stopServer() {
+        server.stopServer();
+    }
+
     @Test
     public void checkEmptyTreeJson() throws Exception {
         LOG.info("checkEmptyTreeJson");
-        final LocalTestServer server = new LocalTestServer("classpath:empty.json");
-        server.startServer();
+        startServer("empty.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertEquals("{}", response.readEntity(String.class));
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        assertEquals("{}", response.readEntity(String.class));
     }
 
     @Test
     public void checkEmptyTreeXml() throws Exception {
         LOG.info("checkSimpleTreeXml");
-        final LocalTestServer server = new LocalTestServer("classpath:empty.json");
-        server.startServer();
+        startServer("empty.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree").
                 request().
-                accept(MediaType.APPLICATION_XML_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><node/>",
+                accept(APPLICATION_XML_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><node/>",
                 response.readEntity(String.class));
 
     }
@@ -52,16 +65,14 @@ public class ApiResponseFormatTest {
     @Test
     public void checkSimpleTreeJson() throws Exception {
         LOG.info("checkSimpleTreeJson");
-        final LocalTestServer server = new LocalTestServer("classpath:simple1.json");
-        server.startServer();
+        startServer("simple1.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertEquals("{\"nodes\":[{\"name\":\"child-1\",\"parameters\":[{\"key\":\"key-1a\",\"value\":\"value-1a\"},{\"key\":\"key-1b\",\"value\":\"value-1b\"}],\"modified\":\"2016-01-02T11:11:11Z\"},{\"name\":\"child-2\",\"parameters\":[{\"key\":\"key-2\",\"value\":\"value-2\"}]}],\"parameters\":[{\"key\":\"key-0\",\"value\":\"value-0\"}],\"modified\":\"2016-01-02T00:00:00Z\",\"levels\":[\"criterium\"]}",
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        assertEquals("{\"nodes\":[{\"name\":\"child-1\",\"parameters\":[{\"key\":\"key-1a\",\"value\":\"value-1a\"},{\"key\":\"key-1b\",\"value\":\"value-1b\"}],\"modified\":\"2016-01-02T11:11:11Z\"},{\"name\":\"child-2\",\"parameters\":[{\"key\":\"key-2\",\"value\":\"value-2\"}]}],\"parameters\":[{\"key\":\"key-0\",\"value\":\"value-0\"}],\"modified\":\"2016-01-02T00:00:00Z\",\"levels\":[\"criterium\"]}",
                 response.readEntity(String.class));
 
     }
@@ -69,16 +80,14 @@ public class ApiResponseFormatTest {
     @Test
     public void checkSimpleTreeXml() throws Exception {
         LOG.info("checkSimpleTreeXml");
-        final LocalTestServer server = new LocalTestServer("classpath:simple1.json");
-        server.startServer();
+        startServer("simple1.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree").
                 request().
-                accept(MediaType.APPLICATION_XML_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><node><nodes><node><name>child-1</name><parameters><parameter><key>key-1a</key><value>value-1a</value></parameter><parameter><key>key-1b</key><value>value-1b</value></parameter></parameters><modified>2016-01-02T11:11:11Z</modified></node><node><name>child-2</name><parameters><parameter><key>key-2</key><value>value-2</value></parameter></parameters></node></nodes><parameters><parameter><key>key-0</key><value>value-0</value></parameter></parameters><modified>2016-01-02T00:00:00Z</modified><levels><level>criterium</level></levels></node>",
+                accept(APPLICATION_XML_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><node><nodes><node><name>child-1</name><parameters><parameter><key>key-1a</key><value>value-1a</value></parameter><parameter><key>key-1b</key><value>value-1b</value></parameter></parameters><modified>2016-01-02T11:11:11Z</modified></node><node><name>child-2</name><parameters><parameter><key>key-2</key><value>value-2</value></parameter></parameters></node></nodes><parameters><parameter><key>key-0</key><value>value-0</value></parameter></parameters><modified>2016-01-02T00:00:00Z</modified><levels><level>criterium</level></levels></node>",
                 response.readEntity(String.class));
 
     }
@@ -86,34 +95,28 @@ public class ApiResponseFormatTest {
     @Test
     public void checkSearchInSImpleTreeJson() throws Exception {
         LOG.info("checkSearchInSImpleTreeJson");
-        final LocalTestServer server = new LocalTestServer("classpath:simple1.json");
-        server.startServer();
+        startServer("simple1.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=criterium&search=child-1").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertEquals("{\"parameters\":[{\"key\":\"key-1a\",\"value\":\"value-1a\"},{\"key\":\"key-1b\",\"value\":\"value-1b\"}],\"matched\":\"/child-1\"}",
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        assertEquals("{\"parameters\":[{\"key\":\"key-1a\",\"value\":\"value-1a\"},{\"key\":\"key-1b\",\"value\":\"value-1b\"}],\"matched\":\"/child-1\"}",
                 response.readEntity(String.class));
-
     }
-
 
     @Test
     public void checkSearchInSimpleTreeXml() throws Exception {
         LOG.info("checkSearchInSimpleTreeXml");
-        final LocalTestServer server = new LocalTestServer("classpath:simple1.json");
-        server.startServer();
+        startServer("simple1.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=criterium&search=child-1").
                 request().
-                accept(MediaType.APPLICATION_XML_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><searchResult><parameters><parameter><key>key-1a</key><value>value-1a</value></parameter><parameter><key>key-1b</key><value>value-1b</value></parameter></parameters><matched>/child-1</matched></searchResult>",
+                accept(APPLICATION_XML_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><searchResult><parameters><parameter><key>key-1a</key><value>value-1a</value></parameter><parameter><key>key-1b</key><value>value-1b</value></parameter></parameters><matched>/child-1</matched></searchResult>",
                 response.readEntity(String.class));
 
     }
@@ -121,60 +124,52 @@ public class ApiResponseFormatTest {
     @Test
     public void checkMultiSearchWrongSeparatorJson() throws Exception {
         LOG.info("checkMultiSearchWrongSeparatorJson");
-        final LocalTestServer server = new LocalTestServer("classpath:simple1.json");
-        server.startServer();
+        startServer("simple1.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=criterium&search=child-1;unknown").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(400, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(400, response.getStatus());
     }
 
     @Test
     public void checkMultiSearchJson() throws Exception {
         LOG.info("checkMultiSearchJson");
-        final LocalTestServer server = new LocalTestServer("classpath:simple1.json");
-        server.startServer();
+        startServer("simple1.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=criterium&search=child-1,unknown").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertEquals("[{\"parameters\":[{\"key\":\"key-1a\",\"value\":\"value-1a\"},{\"key\":\"key-1b\",\"value\":\"value-1b\"}],\"matched\":\"/child-1\"},{\"parameters\":[{\"key\":\"key-0\",\"value\":\"value-0\"}],\"matched\":\"/\"}]",
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        assertEquals("[{\"parameters\":[{\"key\":\"key-1a\",\"value\":\"value-1a\"},{\"key\":\"key-1b\",\"value\":\"value-1b\"}],\"matched\":\"/child-1\"},{\"parameters\":[{\"key\":\"key-0\",\"value\":\"value-0\"}],\"matched\":\"/\"}]",
                 response.readEntity(String.class));
     }
 
     @Test
     public void checkMultiSearchWrongSeparatorXml() throws Exception {
         LOG.info("checkMultiSearchWrongSeparatorXml");
-        final LocalTestServer server = new LocalTestServer("classpath:simple1.json");
-        server.startServer();
+        startServer("simple1.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=criterium&search=child-1;unknown").
                 request().
-                accept(MediaType.APPLICATION_XML_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(400, response.getStatus());
+                accept(APPLICATION_XML_TYPE).get();
+        assertNotNull(response);
+        assertEquals(400, response.getStatus());
     }
 
     @Test
     public void checkMultiSearchXml() throws Exception {
         LOG.info("checkMultiSearchXml");
-        final LocalTestServer server = new LocalTestServer("classpath:simple1.json");
-        server.startServer();
+        startServer("simple1.json");
         final Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=criterium&search=child-1,unknown").
                 request().
-                accept(MediaType.APPLICATION_XML_TYPE).get();
-        server.stopServer();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
-        Assert.assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><searchResults><searchResult><parameters><parameter><key>key-1a</key><value>value-1a</value></parameter><parameter><key>key-1b</key><value>value-1b</value></parameter></parameters><matched>/child-1</matched></searchResult><searchResult><parameters><parameter><key>key-0</key><value>value-0</value></parameter></parameters><matched>/</matched></searchResult></searchResults>",
+                accept(APPLICATION_XML_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><searchResults><searchResult><parameters><parameter><key>key-1a</key><value>value-1a</value></parameter><parameter><key>key-1b</key><value>value-1b</value></parameter></parameters><matched>/child-1</matched></searchResult><searchResult><parameters><parameter><key>key-0</key><value>value-0</value></parameter></parameters><matched>/</matched></searchResult></searchResults>",
                 response.readEntity(String.class));
     }
 }
--- src/test/java/com/tomtom/services/configuration/implementation/RegexConfigTest.java
+++ src/test/java/com/tomtom/services/configuration/implementation/RegexConfigTest.java
@@ -13,6 +13,9 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static org.junit.Assert.*;
+
 @SuppressWarnings({"JUnitTestMethodWithNoAssertions", "OverlyBroadThrowsClause", "ProhibitedExceptionDeclared"})
 public class RegexConfigTest {
     private static final Logger LOG = LoggerFactory.getLogger(RegexConfigTest.class);
@@ -26,108 +29,108 @@ public class RegexConfigTest {
         Response response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=XY12345678/nl/wifi/17.0").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
         String s = response.readEntity(String.class);
         LOG.info("response={}", s);
-        Assert.assertTrue(s.contains("for specific device with MUID XY12345678"));
+        assertTrue(s.contains("for specific device with MUID XY12345678"));
 
         // Search for specific MUID.
         response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=XY12345678/uk/bt/16.4").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
         s = response.readEntity(String.class);
         LOG.info("response={}", s);
-        Assert.assertTrue(s.contains("for specific device with MUID XY12345678"));
+        assertTrue(s.contains("for specific device with MUID XY12345678"));
 
         // Search with too few items specified; should return 404.
         response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=nomuid").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(404, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(404, response.getStatus());
 
         // Search with enough items, but nothing is found; provides fallback.
         response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=nomuid/country/connect/navkit").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
         s = response.readEntity(String.class);
         LOG.info("response={}", s);
-        Assert.assertTrue(s.contains("general fallback"));
+        assertTrue(s.contains("general fallback"));
 
         // Search for config in UK.
         response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=nomuid/UK/connect/navkit").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
         s = response.readEntity(String.class);
         LOG.info("response={}", s);
-        Assert.assertTrue(s.contains("for UK"));
+        assertTrue(s.contains("for UK"));
 
         // Search for config in UK, even though BT and 16.4 is specified.
         response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=nomuid/UK/BT/16.4").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
         s = response.readEntity(String.class);
         LOG.info("response={}", s);
-        Assert.assertTrue(s.contains("for UK"));
+        assertTrue(s.contains("for UK"));
 
         // Same, but now with lowercase 'uk'.
         response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=nomuid/uk/BT/16.4").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
         s = response.readEntity(String.class);
         LOG.info("response={}", s);
-        Assert.assertTrue(s.contains("for UK"));
+        assertTrue(s.contains("for UK"));
 
         // Search for config of BT.
         response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=nomuid/NL/BT/16.4").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
         s = response.readEntity(String.class);
         LOG.info("response={}", s);
-        Assert.assertTrue(s.contains("for bluetooth"));
+        assertTrue(s.contains("for bluetooth"));
 
         // Search for config of 16.4.
         response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=nomuid/x/x/16.4").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
         s = response.readEntity(String.class);
         LOG.info("response={}", s);
-        Assert.assertTrue(s.contains("for 16.4"));
+        assertTrue(s.contains("for 16.4"));
 
         // Search for config of variant of 16.4.
         response = new ResteasyClientBuilder().build().
                 target(server.getHost() + "/tree?levels=deviceID/country/connection/navkit&search=nomuid/x/x/16.4.100").
                 request().
-                accept(MediaType.APPLICATION_JSON_TYPE).get();
-        Assert.assertNotNull(response);
-        Assert.assertEquals(200, response.getStatus());
+                accept(APPLICATION_JSON_TYPE).get();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
         s = response.readEntity(String.class);
         LOG.info("response={}", s);
-        Assert.assertTrue(s.contains("for 16.4"));
+        assertTrue(s.contains("for 16.4"));
 
         server.stopServer();
     }
